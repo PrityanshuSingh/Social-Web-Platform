fbcbded01f9b04aa2e555309c317edbb656dfa1c
Shubham Prakash <prakashshubham26@gmail.com>
2024-05-27 10:04:53 +0530

    Updated Feed Desktop Responsiveness

--- src/components/Feed/FeedPost/FeedPost.jsx
+++ src/components/Feed/FeedPost/FeedPost.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Feed from "./styles/FeedPost.module.css";
-import testfeed from "../../../data/TestFeed.json";
+import testfeed from "../../../Data/testfeed.json";
 
 function FeedFrame() {
 
